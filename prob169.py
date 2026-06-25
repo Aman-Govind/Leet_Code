1b64c8fff1be668d832e4e4433c0292899b429f4
AmanGovind <amangovind48@gmail.com>
2026-06-25 08:55:48 +0530

    Majority Element

---
+++ prob169.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in nums:
+            if nums.count(i)>len(nums)//2:
+                return i
+            
