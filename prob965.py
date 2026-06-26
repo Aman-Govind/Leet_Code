7ef9373cc2179564f52d9748e7b2ac350c9aed57
AmanGovind <amangovind48@gmail.com>
2026-06-26 16:27:11 +0530

    Unvalued Binary Tree

---
+++ prob965.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isUnivalTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        stack=[root]
+        while stack:
+
+            current=stack.pop()
+            if not current:
+                continue
+            if current.val != root.val:
+                return False
+            stack.append(current.right)
+            stack.append(current.left)
+        return True
